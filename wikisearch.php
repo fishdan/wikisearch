43fe1ff187e4132396439091ceb94efdb26e0585
fishdan <fishdan@yahoo.com>
2025-09-19 16:17:26 -0400

    version 0.1.14

--- wikisearch.php
+++ wikisearch.php
@@ -3,7 +3,7 @@
 Plugin Name: WikiSearch
 Plugin URI: https://fishdan.com/wikisearch
 Description: Transforms specified hyperlinks into interactive elements for Wikipedia searches.
-Version: 0.1.3
+Version: 0.1.4
 Author: Dan Fishman
 Author URI: https://fishdan.com
 License: MIT
